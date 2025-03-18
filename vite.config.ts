d29a153797f3d428d3a8d0497e5fe37d101552ab
lalithnarayan <lalith1403@gmail.com>
2025-03-18 21:55:41 +0530

    update website content

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/caffeinebrain/', // Base path for GitHub Pages
+  base: '/caffeinebrain/',
+  assetsInclude: ['**/*.md']  // Add this line to handle markdown files
 })
